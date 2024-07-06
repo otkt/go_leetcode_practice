d38a834b46101d34ef4141cfbaafa4ac9b3c8afc
ozay <ozay1414@gmail.com>
2024-07-06 04:02:29 +0300

    add daily

---
+++ pass_pill.go
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/pass-the-pillow/
+
+package main
+
+//lint:ignore U1000 I
+func passThePillow(n int, time int) int {
+	mod := 2*n - 2
+	rt := time % mod //cycle repeats per rt
+	if rt < n {
+		return rt + 1 // if it doesnt overflow just return
+	} else {
+		return n - (rt % (n - 1)) //if overflow go back from n reverse
+	}
+}
+
+func main() {
+	print(passThePillow(5, 10))
+}
